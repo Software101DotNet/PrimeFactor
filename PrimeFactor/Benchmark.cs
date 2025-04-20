510123e813b2d8de264e8500c3425b75ac138efe
Anthony (MM-M2P) <software101.net@gmail.com>
2025-04-20 13:48:19 +1000

    Update to benchmark.

--- PrimeFactor/Benchmark.cs
+++ PrimeFactor/Benchmark.cs
@@ -9,23 +9,22 @@ namespace PrimeFactor;
 
 public class Benchmark
 {
-    /// Method to messuring the perform of a computer platform
-    /// MacBook Air M4 Benchmarking 10,000,000 primes. Time to compute 14s,878ms 
-    /// Mac Mini M2P Benchmarking 10,000,000 primes. Time to compute 17s,457ms 
-    public static void Serial10M()
-    {
-        uint limit = 10_000_000;
-        Console.Write($"Benchmarking {limit:N0} primes. ");
+	/// Method to messuring the Prime Generation performance of a computer platform.
+	/// Past runs on the following platforms:
+	/// MacBook Air M4 Benchmarking 10,000,000 primes. Time to compute 14s,878ms 
+	/// Mac Mini M2P Benchmarking 10,000,000 primes. Time to compute 17s,457ms 
+	public static void Serial10M()
+	{
+		const uint limit = 10_000_000;
+		Console.Write($"Benchmarking {limit:N0} primes. ");
 
-        // no need to write the resulting calculations to the screen or a file
-        StreamWriter writer = new StreamWriter(Stream.Null);
+		Stopwatch stopWatch = new Stopwatch();
+		stopWatch.Start();
 
-        Stopwatch stopWatch = new Stopwatch();
-        stopWatch.Start();
+		// using a null stream as there is no need to write the resulting calculations to a screen or file.
+		Prime.GeneratePrimes(writer: new StreamWriter(Stream.Null), maxIndex: limit);
 
-        var results = Prime.GeneratePrimes(writer, limit);
-
-        stopWatch.Stop();
-        Console.WriteLine("Time to compute ".FormatTimeSpan(stopWatch.Elapsed));
-    }
+		stopWatch.Stop();
+		Console.WriteLine("Time to compute ".FormatTimeSpan(stopWatch.Elapsed));
+	}
 }
